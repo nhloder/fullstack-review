b73d98ce3678506c82ee61fdaff4f347f12b98d0
nhloder <nhloder@gmail.com>
2019-11-14 11:17:16 -0700

    justTheStart

--- __instructor-code__/src/App.js
+++ __instructor-code__/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import routes from './routes';
+import routes from './routes'
 import Header from './components/Header'
 
 function App() {
